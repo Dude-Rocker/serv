6f2ed4bd5631c1405e463f1e57ef1c8667efeabb
Vyacheslav Gladush <realslavko@gmail.com>
2019-03-29 17:01:38 +0200

    test broad
    Signed-off-by: Vyacheslav Gladush <realslavko@gmail.com>

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void msg(const std::string &s, boost::asio::ip::address ip)
 int main()
 {
     boost::asio::io_service serv;
-    
+
     udp::endpoint receiver_endpoint(udp::v4(), 15);
     Udp_asyn s(serv, receiver_endpoint);
 
@@ -21,18 +21,24 @@ int main()
         serv.run();
     });
 
-    std::string br = "are you hear me?";
-    s.broad_msg(br, 15);
-    sleep(2);
-    br = "yes i hear myself!";
-    s.broad_msg(br, 15);
-    sleep(3);
+    std::string br = "this is broadcast";
+    while (1)
+    {
+        s.broad_msg(br, 15);
+        sleep(5);
+    }
+    
+    #if 0 //test multi
     std::set<udp::endpoint> sp;
-
     sp.insert(udp::endpoint(udp::v4(), 2003));
     sp.insert(udp::endpoint(udp::v4(), 2001));
+    sp.insert(udp::endpoint(udp::v4(), 15));
+    sp.insert(udp::endpoint(boost::asio::ip::address::from_string("192.168.0.116"), 15));
+    sp.insert(udp::endpoint(boost::asio::ip::address::from_string("192.168.0.184"), 15));
+    sp.insert(udp::endpoint(boost::asio::ip::address::from_string("192.168.0.184"), 2001));
     s.multi_msg(sp, "multi testing");
     t.join();
+    #endif
 
     return 0;
 }
